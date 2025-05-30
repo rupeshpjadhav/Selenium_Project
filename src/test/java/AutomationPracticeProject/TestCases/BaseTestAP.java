da7bbb619e24412ba46d51429954a48511cd90fc
Rupesh Jadhav <rupeshpjadhav@gmail.com>
2025-05-30 14:06:28 +0530

    Made some small corrections

--- src/test/java/AutomationPracticeProject/TestCases/BaseTestAP.java
+++ src/test/java/AutomationPracticeProject/TestCases/BaseTestAP.java
@@ -2,13 +2,11 @@ package AutomationPracticeProject.TestCases;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
